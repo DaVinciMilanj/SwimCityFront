ea799d32584dd520e9cd763dc06b520a2dd20627
DaVinciMilanj <radinmilani2005@gmail.com>
2025-03-18 13:24:16 +0330

    serv

--- src/app/accounts/teacher-form/teacher-form.component.ts
+++ src/app/accounts/teacher-form/teacher-form.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { HttpService } from '../../service/http.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-teacher-form',
   standalone: true,
-  imports: [CommonModule , FormsModule , RouterModule , RouterLink],
+  imports: [CommonModule , FormsModule , RouterModule ],
   templateUrl: './teacher-form.component.html',
   styleUrl: './teacher-form.component.css'
 })
@@ -21,19 +21,24 @@ export class TeacherFormComponent {
   successMessage = '';
   errorMessage = '';
 
-  constructor(private _service: HttpService) {}
+  constructor(private _service: HttpService , private router: Router) {}
 
   onSubmit() {
     
     this._service.teacherForm(this.formData).subscribe(
       (response) => {
-        this.successMessage = 'Form submitted successfully!';
+        this.successMessage = 'در خواست شما با موفقیت ثبت شد.';
         this.errorMessage = '';
+        setTimeout(() => {
+          this.router.navigate(['/']); // مسیر مورد نظر خود را جایگزین کنید
+        }, 2000);
       },
+        
       (error) => {
         console.error('Error:', error);
-        this.errorMessage = 'Failed to submit the form. Please check your input.';
+        this.errorMessage = 'لطفا همه فیلد هارا پر کنید';
         console.log(this.formData);
+
         
       }
       
